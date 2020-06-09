cf5d399168bd9b2b28ec9227cffc0e8f7b113495
Julian Kandlhofer <julian.kandlhofer@gmail.com>
2020-06-09 09:21:00 +0200

    change misnamed function name
    Signed-off-by: Julian Kandlhofer <julian.kandlhofer@gmail.com>

--- ratbagd/ratbagd-device.c
+++ ratbagd/ratbagd-device.c
@@ -218,13 +218,13 @@ static int ratbagd_device_commit(sd_bus_message *m,
 }
 
 static int
-ratbag_device_get_model(sd_bus *bus,
-			   const char *path,
-			   const char *interface,
-			   const char *property,
-			   sd_bus_message *reply,
-			   void *userdata,
-			   sd_bus_error *error)
+ratbagd_device_get_model(sd_bus *bus,
+			 const char *path,
+			 const char *interface,
+			 const char *property,
+			 sd_bus_message *reply,
+			 void *userdata,
+			 sd_bus_error *error)
 {
 	struct ratbagd_device *device = userdata;
 	struct ratbag_device *lib_device = device->lib_device;
@@ -245,7 +245,7 @@ ratbag_device_get_model(sd_bus *bus,
 
 const sd_bus_vtable ratbagd_device_vtable[] = {
 	SD_BUS_VTABLE_START(0),
-	SD_BUS_PROPERTY("Model", "s", ratbag_device_get_model, 0, SD_BUS_VTABLE_PROPERTY_CONST),
+	SD_BUS_PROPERTY("Model", "s", ratbagd_device_get_model, 0, SD_BUS_VTABLE_PROPERTY_CONST),
 	SD_BUS_PROPERTY("DeviceType", "u", ratbagd_device_get_device_type, 0, SD_BUS_VTABLE_PROPERTY_CONST),
 	SD_BUS_PROPERTY("Name", "s", ratbagd_device_get_device_name, 0, SD_BUS_VTABLE_PROPERTY_CONST),
 	SD_BUS_PROPERTY("Profiles", "ao", ratbagd_device_get_profiles, 0, SD_BUS_VTABLE_PROPERTY_CONST),
